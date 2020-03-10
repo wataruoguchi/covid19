0908fd20842941d95dd9fee9bb5aeeb7c2f8cfa6
mame3619 <mame3619@gmail.com>
2020-03-11 02:00:06 +0900

    陰性確認数をわかりやすい表現に変更しました

--- pages/index.vue
+++ pages/index.vue
@@ -25,14 +25,14 @@
       </v-col>
       <v-col cols="12" md="6" class="DataCard">
         <time-bar-chart
-          title="陰性確認数"
+          title="治療終了者数"
           :chart-data="dischargesGraph"
           :date="fromdatatodate(dischargesSummary.last_update)"
           sourceFrom="北海道庁webサイト"
           sourceLink="http://www.pref.hokkaido.lg.jp/ss/tkk/singatakoronahaien.htm"
           :unit="'人'"
           :defaultDataKind="'cumulative'"
-          :supplement="'陰性確認数とは陽性の患者が軽快してから48時間後の1回目のPCR検査で陰性が確認され、それから12時間後の2回目のPCR検査でも陰性が確認された方の人数です（3/9 鈴木知事のツイートより）'"
+          :supplement="'治療終了者数とは道発表の「陰性確認済累計」と同じものです。「陰性確認済累計」とは、陽性の患者が軽快してから48時間後の1回目のPCR検査で陰性が確認され、それから12時間後の2回目のPCR検査でも陰性が確認された方の累計のことです。（3/9 鈴木北海道知事のツイートを引用）'"
         />
       </v-col>
       <v-col cols="12" md="6" class="DataCard">
