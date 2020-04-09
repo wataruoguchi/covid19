7b76d9a2cad7a69a5d60e0abdbd6cfed7d793f2c
kohei ikeda <tdn.suama@gmail.com>
2020-04-09 11:44:01 +0900

    print.vueのURL変更(今の所未使用みたいだが…)

--- layouts/print.vue
+++ layouts/print.vue
@@ -32,7 +32,7 @@
                 ※最新の情報はWebページをご覧ください
               </p>
               <p class="PrintMeta-Link">
-                https://stop-covid19-nagano.netlify.com/
+                https://nagano.stopcovid19.jp/
               </p>
             </v-card>
           </v-card>
